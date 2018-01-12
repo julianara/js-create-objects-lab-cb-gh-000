69e9d16a5f9efe20ea2a92a46e9ed299be061e82
Julia Nara <julianara888@gmail.com>
2018-01-12 01:48:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 'use strict';
+
+function Dog(name, breed, age) {
+	this.name = name;
+	this.breed = breed;
+	this.age = age;
+}
+
+function Cat(name, breed, age) {
+	this.name = name;
+	this.breed = breed;
+	this.age = age;
+}
+
+function Ferret(name, breed, foodReq) {
+	this.name = name;
+	this.breed = breed;
+	this.foodReq = foodReq;
+}
+
+function Bird(name, breed) {
+	this.name = name;
+	this.breed = breed;
+}
+
+function Fish(name, species, waterCond) {
+	this.name = name;
+	this.species = species;
+	this.waterCond = waterCond;
+}
